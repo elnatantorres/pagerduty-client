0aa017bcaef680adec49c96e48586c3b419e8c69
lilithlarose <lilith.larose@thoughtworks.com>
2021-06-29 16:09:29 +1000

    Update json property names to reflect PagerDuty api documentation

--- src/Events/ContextProperties/Link.cs
+++ src/Events/ContextProperties/Link.cs
@@ -1,4 +1,6 @@
-﻿namespace PagerDuty.Events.ContextProperties
+﻿using Newtonsoft.Json;
+
+namespace PagerDuty.Events.ContextProperties
 {
     /// <summary>
     /// This property is used to attach text links to the incident.
@@ -8,11 +10,13 @@
         /// <summary>
         /// URL of the link to be attached.
         /// </summary>
+        [JsonProperty(PropertyName = "href")]
         public string HypertextReference { get; set; }
 
         /// <summary>
         /// Plain text that describes the purpose of the link, and can be used as the link's text.
         /// </summary>
+        [JsonProperty(PropertyName = "text")]
         public string Text { get; set; }
     }
 }
